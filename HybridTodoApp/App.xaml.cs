9e0c48779b621b985ab85b8bc45b7aa3cff2ceb4
James Montemagno <jamont@microsoft.com>
2023-08-16 12:25:22 -0700

    Add project files.

---
+++ HybridTodoApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HybridTodoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ HybridTodoApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HybridTodoApp
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
